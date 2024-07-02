bf725199896b2615b3297e8231f8acdc7e687961
your name <shafiarnob@gmail.com>
2024-07-03 02:38:15 +0600

    market bar in trade page

--- app/trade/[market]/page.tsx
+++ app/trade/[market]/page.tsx
@@ -1,12 +1,16 @@
+"use client";
+import { MarketBar } from "@/app/components/MarketBar";
+import { useParams } from "next/navigation";
 import React from "react";
 
 type Props = {};
 
-const page = (props: Props) => {
+const Page = (props: Props) => {
+  const { market } = useParams();
   return (
     <div className="flex flex-row flex-1">
       <div className="flex flex-col flex-1">
-        <p>Market Bar</p>
+        <MarketBar market={market as string} />
         <div className="flex flex-row h-[620px] border-y border-slate-800">
           <div className="flex flex-col flex-1">
             <p>Charts</p>
@@ -27,4 +31,4 @@ const page = (props: Props) => {
   );
 };
 
-export default page;
+export default Page;
